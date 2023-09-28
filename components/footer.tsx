5d5b7ee6c554f6dfd5c0d2527f80dd14cf44910a
Thoriq Ichsani <108530206+RikChat@users.noreply.github.com>
2023-09-28 21:03:13 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,12 +12,9 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://vercel.com/storage/kv">
-        Vercel KV
-      </ExternalLink>
-      .
+      RikChatbot built with{' '}
+      <ExternalLink href="https://rikdevapps.thoriqproject.repl.co">Thoriq Ichsani</ExternalLink>
+      
     </p>
   )
 }
